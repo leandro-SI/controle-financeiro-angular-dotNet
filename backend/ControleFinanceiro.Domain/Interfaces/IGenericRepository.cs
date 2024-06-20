9d5f4c8e4271b7d6eb8ce47bca593fb8e2ddc27f
Leandro <leandro.almeida@uvvnet.com.br>
2024-06-20 14:15:21 -0300

    criando repositorio generico

---
+++ backend/ControleFinanceiro.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using ControleFinanceiro.Domain.Entities;
+
+namespace ControleFinanceiro.Domain.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task Create(T entity);
+        Task<T> FindById(int id);
+        Task<IEnumerable<T>> FindAll();
+        Task Update(T entity);
+        Task Delete(int id);
+    }
+}
---
+++ backend/ControleFinanceiro.Infra.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using ControleFinanceiro.Domain.Entities;
+using ControleFinanceiro.Domain.Interfaces;
+using ControleFinanceiro.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleFinanceiro.Infra.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<T>> FindAll()
+        {
+            var entities = await _context.Set<T>().ToListAsync();
+            return entities;
+        }
+
+        public async Task<T> FindById(int id)
+        {
+            var entity = await _context.Set<T>().FindAsync(id);
+            return entity;
+        }
+
+        public async Task Create(T item)
+        {
+            await _context.Set<T>().AddAsync(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await FindById(id);
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            var registro = _context.Set<T>().Update(entity);
+            registro.State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
