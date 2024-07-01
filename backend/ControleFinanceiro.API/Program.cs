cf3abd9ddf7af4d69e943b32e506b170b150b19b
leandro-si <leandro.almeida@uvvnet.com.br>
2024-07-01 00:19:00 -0300

    configure swagger

--- backend/ControleFinanceiro.API/Program.cs
+++ backend/ControleFinanceiro.API/Program.cs
@@ -1,11 +1,10 @@
-using ControleFinanceiro.Domain.Account;
 using ControleFinanceiro.Domain.Entities;
 using ControleFinanceiro.Infra.Data.Context;
 using ControleFinanceiro.Infra.IoC;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,7 +17,24 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Controle Financeiro API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Leandro Cesar de Almeida",
+            Email = "maildoleo2018@gmail.com",
+            Url = new Uri("https://www.linkedin.com/in/leandro-cesar-de-almeida-68844b14b/")
+        }
+    });
+
+    var xmlFile = "ControleFinanceiro.API.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddIdentity<Usuario, Funcao>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
